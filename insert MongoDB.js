125e99b3d1b7f31f8a4e3b096b58a607de4f1515
Sunil Kumar Chakravartty <91359079+sunil2010040@users.noreply.github.com>
2023-01-23 12:57:25 +0530

    Add files via upload

---
+++ insert MongoDB.js
@@ -0,0 +1,5 @@
+//inserting code in mongodb
+db.items.insertone({"name":"Mi note 7s","price":"10000","rating" :"4","quantity":"400","sold":"320"})
+                
+db.items.insertMany([{"name":"Mi note 7s","price":"10000","rating" :"4","quantity":"400","sold":"320"},{"name":"Mi note 4","price":"8000","rating" :"3","quantity":"300","sold":"220"},{"name":"Mi note 3","price":"7000","rating" :"3.5","quantity":"200","sold":"120","review by":"60"}])
+
